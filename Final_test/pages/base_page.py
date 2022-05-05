44632bb6c0b3865dcc7f8e2634cb76b354203f09
StillTheBest <55080934+LightLisA@users.noreply.github.com>
2022-05-05 22:43:32 +0300

    base_page:      add new line in __init__; add new def is_element_present main_page:      modify like under def should_be_login_link test_main_page: add line - page.should_be_login_link

--- Final_test/pages/base_page.py
+++ Final_test/pages/base_page.py
@@ -1,9 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         # Конструктор класса.
         self.browser = browser          # :param browser:
         self.url = url                  # :param url:
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         """ Метод открывает нужную страницу, используя метод get()"""
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
