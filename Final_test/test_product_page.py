5ac89b8ca8e4f133af3d80df1fd5c5a0d16bbb72
StillTheBest <55080934+LightLisA@users.noreply.github.com>
2022-05-13 00:28:26 +0300

    add "masiv" + loop + comments

--- Final_test/test_product_page.py
+++ Final_test/test_product_page.py
@@ -4,14 +4,20 @@ from pages.product_page import PageObject
 
 masiv = [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason="Some Bug")) for i in range(10)]
 # masiv = [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail(reason="some bug")), 8, 9])
-"""product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-bad_links = [7] #допустим падают тесты 1 и 2
-urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10) if no not in bad_links]
-bad_urls = [f"{product_base_link}/?promo=offer{no}" for no in bad_links]
+
+"""base_url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+url_list = [base_url + "?promo=offer" + str(n) for n in range(0, 10)]  # promo=offerN has bug, will mark it as xfail
+url_list_with_xfail = [pytest.param(url, marks=pytest.mark.xfail) if url[-1] == "N" else url for url in url_list]
 
 
-@pytest.mark.parametrize('ids', urls+list(map(lambda mn: \
-pytest.param(mn, marks=pytest.mark.xfail(strict=True)), bad_urls)))"""
+@pytest.mark.parametrize('url', url_list_with_xfail)
+def test_guest_can_add_product_to_basket(browser, url):
+    # функция
+
+Комментарии:
+1.  url_list                         :    Сначала генерируем список URL страниц
+2. url_list_with_xfail  :    Создаем новый список на основе первого, где помечаем URL, на котором тест падает, как xfail (как в конце этого урока). Не забудьте подставить нужный N :) Подробнее о методе: Finding and replacing elements in a list
+3. Запускаем тест!"""
 
 @pytest.mark.parametrize('ids', masiv)
 def test_guest_can_add_product_to_basket(browser, ids):
