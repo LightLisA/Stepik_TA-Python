7a1db333e5b68e257f63faa16db020a3a8fe78d2
StillTheBest <55080934+LightLisA@users.noreply.github.com>
2022-05-13 00:24:45 +0300

    add "masiv" + loop + comments

--- Final_test/test_product_page.py
+++ Final_test/test_product_page.py
@@ -4,10 +4,16 @@ from pages.product_page import PageObject
 
 masiv = [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason="Some Bug")) for i in range(10)]
 # masiv = [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail(reason="some bug")), 8, 9])
+"""product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+bad_links = [7] #допустим падают тесты 1 и 2
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10) if no not in bad_links]
+bad_urls = [f"{product_base_link}/?promo=offer{no}" for no in bad_links]
 
 
-@pytest.mark.parametrize('ids',
-                         masiv)
+@pytest.mark.parametrize('ids', urls+list(map(lambda mn: \
+pytest.param(mn, marks=pytest.mark.xfail(strict=True)), bad_urls)))"""
+
+@pytest.mark.parametrize('ids', masiv)
 def test_guest_can_add_product_to_basket(browser, ids):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{ids}"
     page = PageObject(browser,
